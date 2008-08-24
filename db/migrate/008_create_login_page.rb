37a254e7196845fef5fd6a9de67e3a9d2adbc3f2
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-08-24 17:24:56 +0200

    Updated installation instructions

--- db/migrate/008_create_login_page.rb
+++ db/migrate/008_create_login_page.rb
@@ -1,5 +1,20 @@
 class CreateLoginPage < ActiveRecord::Migration
   def self.up
+    begin
+      home_page = Page.create!(
+        :title => "Home Page",
+        :breadcrumb => "Home Page",
+        :slug => "/",
+        :status => Status[:published],
+        :published_at => Time.now.to_s(:db)
+      )
+    rescue => e
+      puts e.message
+      puts e.backtrace
+      raise e
+      # It already exists...
+    end
+
     LoginPage.create!
   end
 
